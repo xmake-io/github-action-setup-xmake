cf5188b310e9ce7f5334b95380bf14d82c32ccab
Opportunity <opportunity@live.in>
2019-09-30 13:04:06 +0800

    fix

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-import * as core from '@actions/core';
-import * as path from 'path';
-import { exec, execSync } from 'child_process';
+const core = require('@actions/core')
+const { exec, execSync } = require('child_process')
 
 async function run() {
     try {
-        const version = core.getInput('xmake-version');
+        const version = core.getInput('xmake-version')
 
         execSync("bash <(curl -fsSL https://raw.githubusercontent.com/tboox/xmake/master/scripts/get.sh)")
 
-        core.addPath("/home/runner/.local/bin");
+        core.addPath("/home/runner/.local/bin")
     } catch (error) {
-        core.setFailed(error.message);
+        core.setFailed(error.message)
     }
 }
 
-run();
+run()
