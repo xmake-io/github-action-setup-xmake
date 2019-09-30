74d9ac9b05c2bca569daea871a6486fd6e9e4c89
Opportunity <opportunity@live.in>
2019-09-30 18:16:56 +0800

    fetch

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const core = require('@actions/core')
 const { exec, execSync } = require('child_process')
+const fetch = require('node-fetch')
+const fs = require('fs')
 
 async function run() {
     try {
         const version = core.getInput('xmake-version')
 
-        execSync("bash <(curl -fsSL https://raw.githubusercontent.com/tboox/xmake/master/scripts/get.sh)")
+        const data = fetch('https://raw.githubusercontent.com/tboox/xmake/master/scripts/get.sh')
+        await fs.promises.mkdir('/tmp/xmake')
+        await fs.promises.writeFile(`/tmp/xmake/get.sh`, await (await data).buffer())
+        execSync(`bash /tmp/xmake/get.sh`)
 
         core.addPath("/home/runner/.local/bin")
     } catch (error) {
