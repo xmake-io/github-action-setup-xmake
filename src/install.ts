75f4c1709e7be1cfac906c1431ab37ba47d6253b
ruki <waruqi@gmail.com>
2025-01-14 23:00:50 +0800

    improve install

--- src/install.ts
+++ src/install.ts
@@ -5,7 +5,7 @@ import { selectVersion } from './versions';
 import { winInstall } from './win-install';
 import { unixInstall } from './unix-install';
 
-export async function installXmake(version: Version): Promise<void> {
+export async function installXmake(): Promise<void> {
     const version = await selectVersion();
     if (os.platform() === 'win32' || os.platform() === 'cygwin') {
         const latest = await selectVersion('latest');
